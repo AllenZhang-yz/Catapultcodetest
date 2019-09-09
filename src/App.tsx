30d88876d528778a281225c65b2234e5f990d680
Allenzhang_yz <allen.zhang018@gmail.com>
2019-09-09 12:43:41 +1000

    first edition commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
+import PostsContainer from "./components/Posts/PostsContainer";
+
+const AppWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #2d2d2d;
+  padding-left: 10px;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppWrapper>
+        <Header />
+        <Navigation />
+        <Route path={`/:subreddit`} component={PostsContainer} />
+      </AppWrapper>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/const/index.ts
@@ -0,0 +1,29 @@
+export const subRedditList = [
+  "askreddit",
+  "news",
+  "funny",
+  "politics",
+  "todayilearned",
+  "worldnews",
+  "publicfreakout",
+  "amitheasshole",
+  "pics",
+  "aww",
+  "tifu",
+  "gaming",
+  "gifs",
+  "insaneparents",
+  "relationship_advice",
+  "memes",
+  "askmen",
+  "interestingasfuck",
+  "jokes",
+  "videos",
+  "watchpeopledieinside",
+  "showerthoughts",
+  "legaladvice",
+  "nintendoswitch",
+  "teenagers"
+];
+
+export const ACCESS_TOKEN = "-tedXUfc7g4W9mu4_0rb54HUyEUk";
